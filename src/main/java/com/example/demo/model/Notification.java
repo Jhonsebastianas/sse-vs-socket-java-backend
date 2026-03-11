0a8f3de0bf8dfaa52f25aba1f9bcd17e79b76a19
Sebastian Agudelo <sebastamriel@gmail.com>
2026-03-10 19:07:48 -0500

    Se elimina lombok

--- src/main/java/com/example/demo/model/Notification.java
+++ src/main/java/com/example/demo/model/Notification.java
@@ -1,15 +1,80 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Notification {
     private String id;
     private String message;
     private String type; // e.g., "SSE" or "SOCKET"
     private long timestamp;
+
+    public Notification() {
+    }
+
+    public Notification(String id, String message, String type, long timestamp) {
+        this.id = id;
+        this.message = message;
+        this.type = type;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Notification that = (Notification) o;
+        return timestamp == that.timestamp &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(message, that.message) &&
+                Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, message, type, timestamp);
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" +
+                "id='" + id + '\'' +
+                ", message='" + message + '\'' +
+                ", type='" + type + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
